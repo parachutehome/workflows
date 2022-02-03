e79ea19263a1b380260037e822e9e9a3a307e2e3
Sean Healy <s@xib.ca>
2022-02-03 16:30:32 -0700

    chore: comment for regex

--- set-version.sh
+++ set-version.sh
@@ -4,6 +4,7 @@ if [ -z "$1" ]
   then
     echo "No version supplied"
   else
+    # find parachutehome/workflows...@main and replace with parachutehome/workflows...@VERSION
     find .github/**/*.yml | xargs sed -i "" "s/\(parachutehome\/workflows.*\)@main$/\1@$1/g"
     echo "Version updated from main"
 fi
