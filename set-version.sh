0daa01f5d8f5254bf16770ac3abd4ee78f1eeaf6
Sean Healy <s@xib.ca>
2022-02-03 16:24:29 -0700

    refactor: match for us

--- set-version.sh
+++ set-version.sh
@@ -4,6 +4,6 @@ if [ -z "$1" ]
   then
     echo "No version supplied"
   else
-    find .github/**/*.yml | xargs sed -i "" "s/@main$/@$1/g"
-    echo "Version updated"
+    find .github/**/*.yml | xargs sed -i "" "s/\(parachutehome\/workflows.*\)@main$/\1@$1/g"
+    echo "Version updated from main"
 fi
